454c3aa731543623ba652a81551be7dac4f495a2
Khairul Imam <ki65559@gmail.com>
2017-06-14 18:38:02 +0800

    add jo

---
+++ joinstring.py
@@ -0,0 +1,9 @@
+kontens = ["ini", "adalah", "konten", "percobaan"]
+
+join_konten = ""
+
+for konten in kontens:
+    join_konten = "{} {}".format(join_konten, konten)
+
+join_konten = join_konten[1:]
+print join_konten
