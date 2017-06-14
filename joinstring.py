4fc6eeb8cda22d8c6f674488dd42ab337ff2d151
Khairul Imam <ki65559@gmail.com>
2017-06-14 19:50:02 +0800

    mantap

--- joinstring.py
+++ joinstring.py
@@ -1,9 +1,6 @@
 kontens = ["ini", "adalah", "konten", "percobaan"]
-
 join_konten = ""
-
 for konten in kontens:
     join_konten = "{} {}".format(join_konten, konten)
-
 join_konten = join_konten[1:]
 print join_konten
